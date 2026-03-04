7a9f41a1465596ba71fa32547732376847a2ed75
Никита <nickshaternik@gmail.com>
2026-03-04 20:22:56 +0300

    Тест

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Домашнее задание №3
  * @author Nikita Shaternik
@@ -5,12 +8,83 @@
  */
 
 public class Lesson3 {
-    static void main() {
 
-        int[] test = {11,2,3,4,5};
+    public static void main(String[] args) {
 
-        for (int i = 0; i < test.length; i++) {
-            System.out.println(test[i]);
+        Scanner scanner = new Scanner(System.in);
+        Tasker tasker = new Tasker();
+
+        while (true) {
+            System.out.println("\n1 - Добавить задачу");
+            System.out.println("2 - Показать все задачи");
+            System.out.println("3 - Удалить задачу");
+            System.out.println("4 - Выйти");
+
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1 -> tasker.addTask();
+                case 2 -> tasker.showTasks();
+                case 3 -> tasker.removeTask();
+                case 4 -> {
+                    System.out.println("Выход...");
+                    return;
+                }
+                default -> System.out.println("Неверный пункт меню");
+            }
         }
     }
 }
+
+class Tasker {
+
+    private ArrayList<String> toDo = new ArrayList<>();
+    private Scanner scanner = new Scanner(System.in);
+
+    public void addTask() {
+        System.out.println("Введите задачу:");
+        String task = scanner.nextLine();
+
+        if (task.isBlank()) {
+            System.out.println("Пустую задачу добавить нельзя!");
+            return;
+        }
+
+        toDo.add(task);
+        System.out.println("Задача добавлена!");
+    }
+
+    public void showTasks() {
+        if (toDo.isEmpty()) {
+            System.out.println("Список задач пуст.");
+            return;
+        }
+
+        for (int i = 0; i < toDo.size(); i++) {
+            System.out.println((i + 1) + ". " + toDo.get(i));
+        }
+    }
+
+    public void removeTask() {
+        if (toDo.isEmpty()) {
+            System.out.println("Удалять нечего.");
+            return;
+        }
+
+        showTasks();
+        System.out.println("Введите номер задачи для удаления:");
+
+        int index = scanner.nextInt();
+        scanner.nextLine();
+
+        if (index < 1 || index > toDo.size()) {
+            System.out.println("Неверный номер задачи!");
+            return;
+        }
+
+        toDo.remove(index - 1);
+        System.out.println("Задача удалена.");
+    }
+}
+
