ec334cd255b03608323e4137659e39f4249cc23d
Никита <nickshaternik@gmail.com>
2026-03-04 19:48:02 +0300

    Задание тест

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -1,5 +1,18 @@
+import java.util.ArrayList;
+
+/**
+ * Домашнее задание №3
+ * @author Nikita Shaternik
+ * 04.03.2026
+ */
+
 public class Lesson3 {
     static void main() {
-        System.out.println("Привет");
+
+        int[] test = {1,2,3,4,5};
+
+        for (int i = 0; i < test.length; i++) {
+            System.out.println(test[i]);
+        }
     }
 }
