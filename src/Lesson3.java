1b37acfb1ab5e20fa1321eba9ac529d4d496ac1c
Никита <nickshaternik@gmail.com>
2026-03-04 19:53:02 +0300

    Тест

--- src/Lesson3.java
+++ src/Lesson3.java
@@ -7,7 +7,7 @@
 public class Lesson3 {
     static void main() {
 
-        int[] test = {1,2,3,4,5};
+        int[] test = {11,2,3,4,5};
 
         for (int i = 0; i < test.length; i++) {
             System.out.println(test[i]);
