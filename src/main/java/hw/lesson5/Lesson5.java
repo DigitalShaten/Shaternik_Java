6058d6fdb91e613980736b0a5667e9aac110f4d4
Никита <nickshaternik@gmail.com>
2026-03-14 11:04:51 +0300

    ADD Задача 1 и задача 2 + доработка интерфейса

--- src/main/java/hw/lesson5/Lesson5.java
+++ src/main/java/hw/lesson5/Lesson5.java
@@ -1,70 +1,105 @@
 package hw.lesson5;
-import java.util.Scanner;
-import javafx.geometry.Insets;
+
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.stage.Stage;
+import javafx.scene.text.TextAlignment;
 
 /**
  * Домашнее задание №5
  *
  * @author Nikita Shaternik
- * 11.03.2026
+ * 14.03.2026
  */
 
 public class Lesson5 {
-    private static Scanner scanner = new Scanner(System.in);
-
-    public static void showTaskScene(Stage stage, String taskTitle, VBox taskContent) {
-        //Повтор layout
-        BorderPane root = new BorderPane();
-
-        // Настройки для верхней фразы
-        Label title = new Label("Привет, Томаш!");
-        root.setTop(title);
-        BorderPane.setAlignment(title, Pos.CENTER);
-        BorderPane.setMargin(title, new Insets(20, 0, 20, 0));
-        title.setFont(Font.font("Arial", FontWeight.BOLD, 28));
-
-        if (taskContent !=null) {
-            root.setCenter(taskContent);
-        }
-
-        //Окно логов
-        TextArea logArea = new TextArea();
-        logArea.setEditable(false);
-        logArea.setPrefHeight(150);
-        logArea.setStyle("-fx-font-family: Consolas; -fx-font-size: 12px;");
-        root.setBottom(logArea);
-
-        // Окно
-        Scene scene = new Scene(root, 800, 600);
-        stage.setScene(scene);
-    }
 
     /*Задача 1:
     1.1 Создать двумерный массив, заполнить его случайными числами.
     1.2 Добавить к каждому значению число, которое пользователь будет вводить с консоли.
     1.3 Найти сумму всех получившихся элементов и вывести в консоль. */
-    public static VBox showTaskOne(TextArea logArea) {
 
-        VBox taskContent = new VBox(15);
-        taskContent.setAlignment(Pos.CENTER);
-        taskContent.setPadding(new Insets(20));
+    public static VBox taskOne(TextArea logArea, Main main) {
 
+        VBox box = new VBox(15);
+        box.setAlignment(Pos.CENTER);
 
-        return taskContent;
+        Label header = new Label("Задание 1");
+        header.setFont(Font.font("Arial", FontWeight.BOLD, 22));
+
+        Label underTitle = new Label("Для проверки введите число и нажмите кнопку \"Выполнить\" ");
+        underTitle.setFont(Font.font("Arial", FontWeight.BOLD, 14));
+
+        Label title = new Label("""
+                                Создать двумерный массив, заполнить его случайными числами.
+                                Добавить к каждому значению число, которое пользователь будет вводить с консоли.
+                                Найти сумму всех получившихся элементов и вывести в консоль.
+                                """);
+        title.setWrapText(true);
+        title.setTextAlignment(TextAlignment.CENTER);
+        title.setAlignment(Pos.CENTER);
+
+        Button run = new Button("Выполнить");
+        Button back = new Button("Назад");
+
+        Label label = new Label("Введите число:");
+        TextField input = new TextField();
+        input.setMaxWidth(200);
+
+        run.setOnAction(e -> {
+            logArea.appendText("Сформирован массив.\n");
+            int[][] arrayOneTask = new int[3][3];
+            int sum = 0;
+
+            //перебираем массив для вывода
+            for (int i = 0; i < arrayOneTask.length; i++) {
+                for (int j = 0; j < arrayOneTask[i].length; j++) {
+
+                    arrayOneTask[i][j] = (int)(Math.random() * 10);
+
+                    logArea.appendText(String.format("%3d", arrayOneTask[i][j]));
+                }
+                logArea.appendText("\n");
+            }
+
+            //назначаем число, которое плюсуется и собираем сумму
+            logArea.appendText("Введите число: ");
+
+            try {
+                int number = Integer.parseInt(input.getText());
+
+                logArea.appendText( number + "\n");
+
+                for (int i = 0; i < arrayOneTask.length; i++) {
+                    for (int j = 0; j < arrayOneTask[i].length; j++) {
+
+                        arrayOneTask[i][j] +=number;
+                        sum += arrayOneTask[i][j];
+
+                        logArea.appendText(String.format("%3d", arrayOneTask[i][j]));
+                    }
+                    logArea.appendText("\n");
+                }
+                logArea.appendText("Сумма = " + sum + "\n\n");
+
+            } catch (Exception ex) {
+                logArea.appendText("Ошибка: нужно ввести число\n");
+            }
+        });
+
+        back.setOnAction(e -> main.backToMenu());
+
+        box.getChildren().addAll(header, title, underTitle, label, input, run, back);
+
+        return box;
     }
-
-
 
 
     /*Задача 2:
@@ -80,10 +115,53 @@ public class Lesson5 {
     B W B W B W B W
     W B W B W B W B
     B W B W B W B W*/
-//    public static void taskTwo() {
-//
-//    }
+    public static VBox taskTwo(TextArea logArea, Main main) {
+        VBox box = new VBox(15);
+        box.setAlignment(Pos.CENTER);
 
+        Label header = new Label("Задание 2");
+        header.setFont(Font.font("Arial", FontWeight.BOLD, 22));
+
+        Label title = new Label("""
+                    Создать программу для раскраски шахматной доски с помощью цикла. Создать
+                    двумерный массив String 8х8. С помощью циклов задать элементам массива значения
+                    B(Black) или W(White).""");
+        title.setWrapText(true);
+        title.setTextAlignment(TextAlignment.CENTER);
+        title.setAlignment(Pos.CENTER);
+
+        Button run = new Button("Выполнить");
+        Button back = new Button("Назад");
+
+        String[][] arrayTwoTask = new String[8][8];
+
+        run.setOnAction(e -> {
+            logArea.appendText("Результат задания:\n");
+            //перебираем массив и назначаем цвет
+            for (int i = 0; i < arrayTwoTask.length; i++) {
+                for (int j = 0; j < arrayTwoTask[i].length; j++) {
+
+                    if ((i+j) % 2 == 0){
+                        arrayTwoTask[i][j] = "W";
+                    } else {
+                        arrayTwoTask[i][j] = "B";
+                    }
+
+                    logArea.appendText(String.format("%2s", arrayTwoTask[i][j]));
+                }
+                logArea.appendText("\n");
+            }
+        });
+
+        back.setOnAction(e -> {
+            main.backToMenu();
+            logArea.clear();
+        });
+
+        box.getChildren().addAll(header, title, run, back);
+
+        return box;
+    }
     /*Задача *:
     Даны числа n и m. Создайте массив A[n][m] и заполните его змейкой (см. пример).
     Формат входных данных:
--- src/main/java/hw/lesson5/Main.java
+++ src/main/java/hw/lesson5/Main.java
@@ -1,81 +1,83 @@
 package hw.lesson5;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
+import javafx.geometry.*;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextArea;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontWeight;
+import javafx.scene.control.*;
+import javafx.scene.layout.*;
+import javafx.scene.text.*;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    private BorderPane root;
+    private TextArea logArea;
 
     @Override
     public void start(Stage stage) {
+        root = new BorderPane();
 
-        BorderPane root = new BorderPane();
-        Lesson5 homework = new Lesson5();
-
-        // Настройки для верхней фразы
         Label title = new Label("Привет, Томаш!");
-        root.setTop(title);
-        BorderPane.setAlignment(title, Pos.CENTER);
-        BorderPane.setMargin(title, new Insets(20, 0, 20, 0));
         title.setFont(Font.font("Arial", FontWeight.BOLD, 28));
+        BorderPane.setAlignment(title, Pos.CENTER);
+        BorderPane.setMargin(title, new Insets(20));
+        root.setTop(title);
 
-        // Окно
+        logArea = new TextArea();
+        VBox bottomBox = new VBox(logArea);
+        logArea.setEditable(false);
+        bottomBox.setPadding(new Insets(10));
+        logArea.setStyle("""
+                    -fx-font-family: Consolas;
+                    -fx-font-size: 13px; 
+                     """);
+        root.setBottom(bottomBox);
+
+        showMainMenu();
+
         Scene scene = new Scene(root, 800, 600);
-
-        //Окно логов
-        TextArea logArea = new TextArea();
-        logArea.setEditable(false); // запрещаем писать в логах
-        logArea.setPrefHeight(150);
-        logArea.setStyle("-fx-font-family: Consolas; -fx-font-size: 12px;"); // нагенерил красоты чуток
-        root.setBottom(logArea);
-
-        //Кнопки
-        HBox buttons = new HBox(10);
-
-        Button taskOneBut = new Button("Задание 1");
-        Button taskTwoBut = new Button("Задание 2");
-        Button taskThreeBut = new Button("Задание 3");
-        Button exit = new Button("Выход");
-        buttons.getChildren().addAll(taskOneBut, taskTwoBut, taskThreeBut, exit);
-        buttons.setAlignment(Pos.CENTER);
-        //Стили кнопочек
-        buttons.setStyle("""
-                    -fx-background-color: #3498db;
-                    -fx-text-fill: white;
-                    -fx-font-size: 14px;
-                """);
-        buttons.setPadding(new Insets(20));
-
-        root.setCenter(buttons);
-
-        VBox taskContent = Lesson5.showTaskOne(logArea);
-
-        // Добавляем обработчик для кнопок
-        taskOneBut.setOnAction(e -> {
-//            logArea.appendText("Нажали Задание 1!\n");
-//            homework.showTaskOne(stage, "Задание 1", taskContent);
-        });
-        taskTwoBut.setOnAction(e -> logArea.appendText("Нажали Задание 2!\n"));
-        taskThreeBut.setOnAction(e -> logArea.appendText("Нажали Задание 3!\n"));
-        exit.setOnAction(e -> {
-            logArea.appendText("Выход...\n");
-            stage.close();
-        });
-
 
         stage.setTitle("Интерфейс для домашнего задания");
         stage.setScene(scene);
         stage.show();
+    }
+
+    private void showMainMenu() {
+
+        Lesson5 tasker = new Lesson5();
+        HBox buttons = new HBox(10);
+        buttons.setAlignment(Pos.CENTER);
+        buttons.setPadding(new Insets(20));
+
+        Button task1 = new Button("Задание 1");
+        Button task2 = new Button("Задание 2");
+        Button task3 = new Button("Задание 3");
+        Button exit = new Button("Выход");
+
+        buttons.getChildren().addAll(task1, task2, task3, exit);
+
+        root.setCenter(buttons);
+
+        task1.setOnAction(e -> {
+            logArea.clear();
+            logArea.appendText("Открыто задание 1\n");
+            root.setCenter(tasker.taskOne(logArea, this));
+        });
+
+        task2.setOnAction(e -> {
+            logArea.clear();
+            logArea.appendText("Открыто задание 2\n");
+            root.setCenter(tasker.taskTwo(logArea, this));
+        });
+        task3.setOnAction(e -> logArea.appendText("Задание 3 пока не сделано\n"));
+
+        exit.setOnAction(e -> {
+            logArea.appendText("Выход...\n");
+            System.exit(0);
+        });
+    }
+
+    public void backToMenu() {
+        showMainMenu();
     }
 
     public static void main(String[] args) {
