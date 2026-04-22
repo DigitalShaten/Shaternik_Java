0bbd00c565be9077591f140b1aee0d1080f5bb6e
Никита <nickshaternik@gmail.com>
2026-04-22 19:23:10 +0300

    ADD class Account для работы со счетом и балансом (методы депозита и снятия денег)

--- src/java_core_final_project/model/Account.java
+++ src/java_core_final_project/model/Account.java
@@ -2,4 +2,22 @@ package java_core_final_project.model;
 
 //счет-баланс
 public class Account {
+    private String accountNumber;
+    private int balance;
+
+    public Account(String accountNumber, int balance) {
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public void deposit (int money) {
+        balance += money;
+        System.out.println("Пополнение счета на: " + money + " руб. На балансе сейчас: " + balance + " руб.");
+    }
+
+    public void withdraw (int money) {
+        if (balance < money) return;
+        balance -= money;
+        System.out.println("Снятие со счета на: " + money + " руб. На балансе сейчас: " + balance + " руб.");
+    }
 }
