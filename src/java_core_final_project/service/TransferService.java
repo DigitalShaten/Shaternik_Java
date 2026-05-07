f6b2611e3326cf3d0ae013df89f97c95e9d07b3f
Никита <nickshaternik@gmail.com>
2026-05-07 21:02:08 +0300

    JCP/ класс для переводов TransferService

--- src/java_core_final_project/service/TransferService.java
+++ src/java_core_final_project/service/TransferService.java
@@ -1,5 +1,56 @@
 package java_core_final_project.service;
 
-// класс для переводов
+import java_core_final_project.exception.AccountNotFoundException;
+import java_core_final_project.exception.InvalidAmountException;
+import java_core_final_project.model.Account;
+import java_core_final_project.model.Transaction;
+import java_core_final_project.model.TransactionStatus;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
 public class TransferService {
+    private final Map<String, Account> accounts;
+
+    public TransferService (Map<String, Account> accounts) {
+        this.accounts = accounts;
+    }
+
+    public Transaction transfer(
+            String fromAccountNumber,
+            String toAccountNumber,
+            int amount
+    ) {
+        try {
+            Account fromAccount = findAccount(fromAccountNumber);
+            Account toAccount = findAccount(toAccountNumber);
+
+            fromAccount.withdraw(amount);
+            toAccount.deposit(amount);
+
+            return new Transaction(
+                    fromAccountNumber,
+                    toAccountNumber,
+                    amount,
+                    TransactionStatus.SUCCESS,
+                    LocalDateTime.now()
+            );
+        } catch (AccountNotFoundException | InvalidAmountException exception){
+            return new Transaction(
+                    fromAccountNumber,
+                    toAccountNumber,
+                    amount,
+                    TransactionStatus.ERROR,
+                    LocalDateTime.now()
+            );
+        }
+    }
+
+    private Account findAccount (String accountNumber) {
+        Account account = accounts.get(accountNumber);
+        if (account == null) {
+            throw new AccountNotFoundException("Аккаунт не найден: " + accountNumber);
+        }
+        return account;
+    }
 }
